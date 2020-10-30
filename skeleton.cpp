146011900bce8ce6d442e9bfd8acb29c21bf3bfe
Randy Heiland <heiland@indiana.edu>
2020-10-30 06:14:44 -0400

    init

---
+++ skeleton.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+class Intra {  // abstract
+  private:
+  public:
+    std::string type;
+    // std::string sbml_filename;
+	
+	// virtual void initialize_intracellular_from_pugixml(pugi::xml_node& node) = 0;
+	virtual void start() = 0;
+};
+class Sbml : public Intra {  // concrete class
+  private:
+  public:
+
+	void start() { cout << "sbml: start()\n"; } 
+	// void initialize_intracellular_from_pugixml(pugi::xml_node& node);
+
+};
+class Pheno {
+  private:
+  public:
+    Intra* imodel;
+
+};
+class Cell {
+  private:
+  public:
+    Pheno p;
+};
+
+int main()
+{
+    Cell c;
+    Sbml s;
+    s.type = "sbml";
+    s.start();
+    c.p.imodel = &s;
+    cout << "c.p.imodel->type = " << c.p.imodel->type << endl;
+}
